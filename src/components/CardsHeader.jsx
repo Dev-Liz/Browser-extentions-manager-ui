d8f7e888e2298ae0df92e386d51c60bf01d846cc
Dev-Liz <basseyelizabeth569@gmail.com>
2026-08-01 11:16:37 +0100

    fix responsive layout.

--- src/components/CardsHeader.jsx
+++ src/components/CardsHeader.jsx
@@ -5,7 +5,7 @@ import ToggleSwitch from "./ToggleSwitch";
 export default function CardsHeader({ filter, setFilter }) {
   return (
     <header className="cards-header">
-      <h1>Extentions List</h1>
+      <h1>Extensions List</h1>
       <Badges filter={filter} setFilter={setFilter} />
     </header>
   );
